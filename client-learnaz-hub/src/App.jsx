21d56b4f786e1da34a19857a49beb30eac410221
Joe Egboka <joeegboka@gmail.com>
2024-07-07 22:59:08 +0100

    create placeholder components and organize code structure

--- client-learnaz-hub/src/App.jsx
+++ client-learnaz-hub/src/App.jsx
@@ -1,11 +1,28 @@
-import LandingPage from "./Pages/LandingPage";
+import { Route, Routes } from "react-router-dom";
+
+import { LandingPage } from "./pages";
+import {
+  Footer,
+  Header,
+  LoginForm,
+  PasswordRecoveryForm,
+  SignupForm,
+} from "./components";
 
 const App = () => {
   return (
     <>
-    <LandingPage/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/password-recovery" element={<PasswordRecoveryForm />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 };
 
 export default App;
---
+++ client-learnaz-hub/src/components/index.jsx
@@ -0,0 +1,7 @@
+import SignupForm from "./forms/SignupForm";
+import LoginForm from "./forms/LoginForm";
+import PasswordRecoveryForm from "./forms/passwordRecoveryForm";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
+export { SignupForm, LoginForm, PasswordRecoveryForm, Header, Footer };
