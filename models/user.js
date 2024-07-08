19583a301e7ccc26ed6596c6c64298b2f785b944
Lamber22 <Lamberjoe5@gmail.com>
2024-07-08 03:34:54 +0000

    database schema created

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const users = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['student', 'teacher'], // Allowed roles
+        default: 'student' // Default role for new users
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('user', users);
