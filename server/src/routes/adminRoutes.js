b7c81845374dc85be2e4f7c7a8365a251552c0c4
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-09 01:59:25 +0200

    Update adminRoutes.js

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getAdminDashboard } = require("../controllers/adminController");
 
-// Example route for admin dashboard
+// route for admin dashboard
 router.get("/dashboard", getAdminDashboard);
 
 module.exports = router;
