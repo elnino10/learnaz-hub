005ed1bdd564823e04c18a110b81f6353666a714
Lamber22 <Lamberjoe5@gmail.com>
2024-07-09 16:02:18 +0000

    Refactor  setup to integrate MongoDB with Mongoose, add course routes

--- server/api/v1/app.js
+++ server/api/v1/app.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const morgan = require("morgan");
+import express from 'express';
+import bodyParser from "body-parser";
+import cors from 'cors';
+import morgan from 'morgan';
+import dbClient from '../../src/utils/db.js';
+import mongoose from 'mongoose';
+import courseRoutes from '../../src/routes/courseRoutes.js';
 
 const app = express();
+
+mongoose.connect(dbClient.url, {
+})
+.then(() => console.log('Mongoose connected successfully'))
+  .catch((error) => console.error('Error connecting to Mongoose:', error));
 
 // third-party middlewares
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(cors());
+app.use('/api/v1', courseRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("Hello! From Learnaz Hub Server\n");
 });
 
-module.exports = app;
+
+export default app;
