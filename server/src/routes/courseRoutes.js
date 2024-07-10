4b75a714c1cc547685b02b819c79587be1f9b9fe
Lamber22 <Lamberjoe5@gmail.com>
2024-07-10 00:40:00 +0000

    removed empty line course route

--- server/src/routes/courseRoutes.js
+++ server/src/routes/courseRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import {
     createCourse,
@@ -17,16 +16,3 @@ router.put('/:id', updateCourse);
 router.delete('/:id', deleteCourse);
 
 export default router;
-=======
-const express = require("express");
-const router = express.Router();
-const { getCourses, createCourse } = require("../controllers/courseController");
-
-// route to get all courses
-router.get("/", getCourses);
-
-// route to create a course
-router.post("/", createCourse);
-
-module.exports = router;
->>>>>>> 726fb60fca7e807a0fc1e23875a2299dbc62b25d
