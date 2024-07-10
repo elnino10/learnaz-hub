2ce5b001dfd1a159441e6a9a65b6c9a12550fcec
Joe Egboka <joeegboka@gmail.com>
2024-07-10 15:25:01 +0100

    update backend file structure and import format

--- server/src/routes/lessonRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getLessons } = require("../controllers/lessonController");
-
-// route to get lessons for a course
-router.get("/:courseId/lessons", getLessons);
-
-module.exports = router;
--- server/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUserProfile } = require("../controllers/userController");
-
-// route to get user profile
-router.get("/profile", getUserProfile);
-
-module.exports = router;
