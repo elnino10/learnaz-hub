d85a6c2ec7ba35ba5b4f9bb16516cf7feca7e020
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-12 01:25:46 +0200

    Update userRoutes.js

--- server/api/v1/routes/userRoutes.js
+++ server/api/v1/routes/userRoutes.js
@@ -1,6 +1,5 @@
-// server/src/api/v1/routes/userRoutes.js
+// server/src/routes/userRoutes.js
 import express from 'express';
-import { check } from 'express-validator';
 import {
   registerUser,
   authUser,
@@ -9,23 +8,9 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/userController.js';
-=========
-// // server/src/api/v1/routes/userRoutes.js
-// import express from "express";
-// import { check } from "express-validator";
-// import {
-//   registerUser,
-//   authUser,
-//   getUsers,
-//   getUserById,
-//   updateUser,
-//   deleteUser,
-// } from "../controllers/userController.js";
->>>>>>>>> Temporary merge branch 2
 
-// const router = express.Router();
+const router = express.Router();
 
-<<<<<<<<< Temporary merge branch 1
 // Route to register a new user
 router.post('/register', registerUser);
 
@@ -35,38 +20,13 @@ router.post('/login', authUser);
 // Route to get all users
 router.get('/', getUsers);
 
+// Route to get a user by ID
 router.get('/:id', getUserById);
 
+// Route to update a user by ID
 router.put('/:id', updateUser);
 
+// Route to delete a user by ID
 router.delete('/:id', deleteUser);
-=========
-// router.post(
-//   "/register",
-//     [
-//       check('name', 'Name is required').not().isEmpty(),
-//       check('email', 'Please include a valid email').isEmail(),
-//       check('password', 'Password is required').isLength({ min: 6 }),
-//     ],
-//   registerUser
-// );
 
-// router.post(
-//   "/login",
-//     [
-//       check('email', 'Please include a valid email').isEmail(),
-//       check('password', 'Password is required').exists(),
-//     ],
-//   authUser
-// );
-
-// router.get("/", getUsers);
-
-// router.get("/:id", getUserById);
-
-// router.put("/:id", updateUser);
-
-// router.delete("/:id", deleteUser);
->>>>>>>>> Temporary merge branch 2
-
-// export default router;
+export default router;
