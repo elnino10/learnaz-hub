24e254ab7187b37e80ff44dc64516b5198309281
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-07-12 00:44:41 +0200

    Update userModel.js

--- server/api/v1/models/userModel.js
+++ server/api/v1/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -45,6 +46,21 @@ const userSchema = new mongoose.Schema({
     default: Date.now(),
   },
   updatedAt: Date,
+});
+
+// Method to match entered password with the hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Pre-save middleware to hash the password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
