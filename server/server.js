1a028d8f28fd35ff0bb066e93880efd0ad4903b3
Joe Egboka <joeegboka@gmail.com>
2024-07-11 13:55:46 +0100

    create a mongodb atlas db and connect to database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-import app from './api/v1/app.js';
+import app from "./api/v1/app.js";
+import "dotenv/config";
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVER_PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
