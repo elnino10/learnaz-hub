a4e9b39c56b9e06b3ad1d573be224adb8e15f3ae
Lamber22 <Lamberjoe5@gmail.com>
2024-07-11 12:03:57 +0000

    Fixed typo in importing jsonwebtoken

--- server/api/v1/controllers/authController.js
+++ server/api/v1/controllers/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jasonwebtoken';
+import jwt from 'jsonwebtoken';
 const secretKey = '__secret_key__';
 
 import User from '../models/user.js';
@@ -11,11 +11,11 @@ export const register = async (req, res) => {
         if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
         const hashPassword = await bcrypt.hash(password, 10);
-        const User = new User({ email, password: hashPassword, });
-        await User.save();
+        const newUser = new User({ email, password: hashPassword, });
+        await newUser.save();
         res.status(201).json({ message: 'User registered successfully!' });
     } catch (error) {
-        res.send(500).json({ message: error });
+        res.status(500).json({ message: error });
     }
 };
 
@@ -28,7 +28,7 @@ export const login = async (req, res) => {
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) return res.status(400).json({ message: 'Password incorrect' });
 
-        const token = jwt.signin({ id: user._id }, secretKey, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, secretKey, { expiresIn: '1h' });
         res.status(200).json({ token, message: 'Login Successful!' });
 
 
