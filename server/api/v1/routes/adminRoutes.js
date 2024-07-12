25ff90341a6a53e28b99d6024c1d68bf7a947989
Lamber22 <Lamberjoe5@gmail.com>
2024-07-12 04:18:15 +0000

    update import in adminRoutes, move adminController to admin folder

--- server/api/v1/routes/adminRoutes.js
+++ server/api/v1/routes/adminRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { registerAdmin, loginAdmin } from '../controllers/adminController.js';
+import { registerAdmin, loginAdmin } from '../controllers/admin/adminController.js';
+import { grantPrivileges, revokePrivileges } from '../controllers/admin/privileges.js';
 import authMiddleware, { isAdmin } from '../utils/authMiddleware.js';
 
 const router = Router();
 
 router.post('/register_admin', registerAdmin);
 router.post('/admin_login', authMiddleware, isAdmin, loginAdmin);
-
+router.post("/grant-privileges", grantPrivileges);
+router.post("/revoke-privileges", revokePrivileges);
 export default router;
