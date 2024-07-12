60559bf0614b73ad5265990d5807e82e9f26839d
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2024-07-12 06:06:50 +0200

    update

--- server/api/v1/controllers/userController.js
+++ server/api/v1/controllers/userController.js
@@ -1,5 +1,5 @@
 // server/src/api/v1/controllers/userController.js
-import User from '../models/userModel.jsgit ';
+import User from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
