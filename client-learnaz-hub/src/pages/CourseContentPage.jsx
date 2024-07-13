89f517ee0b0d597eb11dd105c581b43999715b18
Joe Egboka <joeegboka@gmail.com>
2024-07-13 22:49:33 +0100

    add features to CourseContectPage

--- client-learnaz-hub/src/pages/CourseContentPage.jsx
+++ client-learnaz-hub/src/pages/CourseContentPage.jsx
@@ -1,7 +1,14 @@
 function CourseContentPage() {
   return (
-    <div className="mt-20">CourseContentPage</div>
-  )
+    <div className="mt-20 px-5 flex">
+      <div className="w-[75%] h-80 bg-gray-100 mb-5">
+        <div>
+          <video src="https://www.youtube.com/watch?v=RM0nfs4IhyU"></video>
+        </div>
+      </div>
+      <div className="">course modules</div>
+    </div>
+  );
 }
 
-export default CourseContentPage
+export default CourseContentPage;
