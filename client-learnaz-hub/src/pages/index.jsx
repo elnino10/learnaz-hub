757349a22a6f9ca252050975c7eac1004ee07a54
Joe Egboka <joeegboka@gmail.com>
2024-07-13 13:22:46 +0100

    add CourseContentPage and change some user route

--- client-learnaz-hub/src/pages/index.jsx
+++ client-learnaz-hub/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import LandingPage from "./LandingPage";
 import EnrolledCoursesPage from "./EnrolledCoursesPage";
 import UserDashboard from "./UserDashboard"
+import CourseContentPage from "./CourseContentPage";
 
-export { LandingPage, EnrolledCoursesPage, UserDashboard };
+export { LandingPage, EnrolledCoursesPage, UserDashboard, CourseContentPage };
