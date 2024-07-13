8f968c43420aa15e98f8515a408518e2d0751401
Lamber22 <Lamberjoe5@gmail.com>
2024-07-13 23:03:03 +0000

    updated the routes

--- server/api/v1/routes/instructorRoutes.js
+++ server/api/v1/routes/instructorRoutes.js
@@ -1,23 +1,12 @@
-// import { Router } from "express";
-// import {
-//   getTutorProfile,
-//   createTutorProfile,
-//   updateTutorProfile,
-//   deleteTutorProfile,
-// } from "../controllers/instructorController.js";
+import express from 'express';
+import {
+    registerInstructor,
+    loginInstructor
+} from '../controllers/instructor/instructorController.js';
 
-// const router = Router();
+const router = express.Router();
 
-// // route to get tutor profile
-// router.get("/:tutorId", getTutorProfile);
+router.post('/register', registerInstructor);
+router.post('/login', loginInstructor);
 
-// // route to create tutor profile
-// router.post("/", createTutorProfile);
-
-// // route to update tutor profile
-// router.put("/:tutorId", updateTutorProfile);
-
-// // route to delete tutor profile
-// router.delete("/:tutorId", deleteTutorProfile);
-
-// export default router;
+export default router;
