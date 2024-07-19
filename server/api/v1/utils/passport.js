e7763860c9e83c819733ad4903d8d21e6850a528
Lamber22 <Lamberjoe5@gmail.com>
2024-07-19 11:27:34 +0000

    Add documentation to passport.js

--- server/api/v1/utils/passport.js
+++ server/api/v1/utils/passport.js
@@ -1,26 +1,35 @@
-import passport from 'passport';
+import passport from 'passport'; // Passport.js authentication middleware
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import LocalStrategy from 'passport-local';
 import mongoose from 'mongoose';
-import User from "../models/userModel.js"
+import User from "../models/userModel.js";
 
+
+ //Configure JWT options
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.JWT_SECRET;
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // Extract JWT from Authorization header
+opts.secretOrKey = process.env.JWT_SECRET; // Secret key for JWT verification
 
+
+//Define JWT strategy for Passport.js
 passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+
+    //Verify JWT payload and find user by ID
     User.findById(jwt_payload.id)
         .then(user => {
-            if (user) {
-                return done(null, user);
-            }
-            return done(null, false);
+        if (user) {
+            return done(null, user);
+        }
+        return done(null, false);
         })
         .catch(err => console.log(err));
-}));
+    }));
 
-passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+
+    //Define Local strategy for Passport.js
+    passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
     try {
+        //Find user by email
         const user = await User.findOne({ email });
 
         if (!user) {
