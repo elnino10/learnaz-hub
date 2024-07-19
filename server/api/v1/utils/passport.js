53e014b761d56f6d8a44e8a3748d402b3b8fa045
Joe Egboka <joeegboka@gmail.com>
2024-07-19 17:09:26 +0100

    add updates to files

--- server/api/v1/utils/passport.js
+++ server/api/v1/utils/passport.js
@@ -1,42 +1,49 @@
-import passport from 'passport';
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-import LocalStrategy from 'passport-local';
-import mongoose from 'mongoose';
-import User from "../models/userModel.js"
+import passport from "passport";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import LocalStrategy from "passport-local";
+import mongoose from "mongoose";
+import User from "../models/userModel.js";
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWT_SECRET;
 
-passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
     User.findById(jwt_payload.id)
-        .then(user => {
-            if (user) {
-                return done(null, user);
-            }
-            return done(null, false);
-        })
-        .catch(err => console.log(err));
-}));
+      .then((user) => {
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      })
+      .catch((err) => console.log(err));
+  })
+);
 
-passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
-    try {
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
         const user = await User.findOne({ email });
 
         if (!user) {
-        return done(null, false);
+          return done(null, false);
         }
 
         const isValid = await user.matchPassword(password);
 
         if (!isValid) {
-        return done(null, false);
+          return done(null, false);
         }
 
         return done(null, user);
-    } catch (error) {
+      } catch (error) {
         return done(error);
+      }
     }
-}));
+  )
+);
 
 export default passport;
