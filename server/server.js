a263ff0753b9ea3032d4e61969d026643b25509c
chimaskyy <chiamakahj@gmail.com>
2024-07-25 07:34:07 +0100

    check responsiveness

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ import app from './api/v1/app.js';
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+app.listen(port,() => {
   console.log(`Server is running on port ${port}`);
 });
